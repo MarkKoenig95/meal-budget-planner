c44b2b65b4b5bf8ab7c130df4be252b509948139
Mark Koenig <markrkoenig95@gmail.com>
2020-01-16 19:59:07 +0800

    Adjustments for deploy

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 const { database, Ingredient } = require("../database");
 
 router
   .route("/")
   .get((req, res) => {
-    res.sendStatus(200);
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   })
   .post((req, res) => {});
 
--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "..", "public")));
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "..", "build")));
 
 ///////////////////////////////////////// Routes /////////////////////////////////////////
 
